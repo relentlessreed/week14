4777f29166ef23804d9733fda32052c2a0dcd0e8
Edward Reed <relentlessreed@gmail.com>
2021-06-22 12:47:41 -0500

    Updated Models

---
+++ models/comment.js
@@ -0,0 +1,33 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    date_created: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Comment",
+  }
+);
+
+module.exports = Comment;
